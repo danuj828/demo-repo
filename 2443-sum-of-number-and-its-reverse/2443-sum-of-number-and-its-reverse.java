15a314a6a55f356c9804e2b9362071ce2724ff12
Anuj Dubey <113243188+idkidgaf@users.noreply.github.com>
2022-10-17 19:21:28 +0530

    Time: 333 ms (100.00%), Space: 41 MB (100.00%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean sumOfNumberAndReverse(int num) {
+        
+        for(int i = 0; i<=num; i++){
+            int x = i;int  y = 0;
+            while(x>0){   
+            y = y * 10 + x%10;  
+            x = x/10;
+            }
+            if(y+i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
