7cc209458cc955429d334a261c726c5a305c0d1e
Anuj Dubey <113243188+idkidgaf@users.noreply.github.com>
2022-10-26 16:35:03 +0530

    Time: 70 ms (27.99%), Space: 108.4 MB (75.27%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,28 +1,50 @@
 class Solution {
+    // public boolean checkSubarraySum(int[] nums, int k) {
+        
+//         int n = nums.length;
+        
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int sum = 0;
+//         if(nums.length < 2){
+//             return false;
+//         }
+        
+//         map.put(0, -1);
+//         for(int i = 0; i<n; i++){
+//             sum += nums[i];
+//             if(k != 0){
+//                 sum = sum % k;
+//             }
+//             if(map.containsKey(sum)){
+//                 if(i - (map.get(sum)) > 1){
+//                     return true;
+//                 }
+//             } else {
+//                 map.put(sum , i);
+//             }
+//         }
+//         return false;
+    // }
+    
     public boolean checkSubarraySum(int[] nums, int k) {
-        
-        int n = nums.length;
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<> ();
         int sum = 0;
-        if(nums.length < 2){
-            return false;
-        }
-        
         map.put(0, -1);
-        for(int i = 0; i<n; i++){
-            sum += nums[i];
-            if(k != 0){
-                sum = sum % k;
-            }
-            if(map.containsKey(sum)){
-                if(i - (map.get(sum)) > 1){
-                    return true;
+        boolean found = false;
+        int n = nums.length;
+        for(int i=0; i< n; i++) {
+            sum = sum + nums[i];
+            int hash = sum % k;
+            if(map.containsKey(hash)) {
+                int diff = i - map.get(hash);
+                if(diff>=2) {
+                    found = true;
+                    break;
                 }
             } else {
-                map.put(sum , i);
+                map.put(hash, i);
             }
         }
-        return false;
+        return found;
     }
 }
