2f226ebdf30135b90875bcfdda1d891ef4050126
Anuj Dubey <113243188+idkidgaf@users.noreply.github.com>
2022-10-27 22:49:23 +0530

    Time: 8 ms (74.92%), Space: 48.6 MB (31.46%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,37 @@
+class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+    
+    public void push(int val) {
+        if(val <= min){
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        int peek = stack.pop();
+        if(peek == min){
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
