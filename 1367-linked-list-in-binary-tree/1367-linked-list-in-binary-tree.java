070f2a26a6f6f2a337dda5df37ac82c569dc02ef
Anuj Dubey <113243188+idkidgaf@users.noreply.github.com>
2022-10-28 12:27:30 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (98.31%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+//     public boolean isSubPath(ListNode head, TreeNode root) {
+        
+//         if(root == null) return false;
+//         if(head == null) return true;
+        
+//         boolean ans = false;
+//         if(root.val == head.val){
+//             ans = findpath(head.next, root.left) || findpath(head.next, root.right);
+//         }
+//         return ans || isSubPath(head, root.left) || isSubPath(head, root.right);
+//     }
+    
+//     public static boolean findpath(ListNode head, TreeNode root){
+//         if(root == null) return false;
+//         if(head == null) return true;
+        
+//         if(head.val == root.val){
+//             return findpath(head.next, root.left) || findpath(head.next, root.right);
+//         } 
+//         return false;
+//     }
+    
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+    private boolean dfs(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        return head.val == root.val && (dfs(head.next, root.left) || dfs(head.next, root.right));
+    }
+    
+}
