1eb24bae66926cea119a3a1bfc257bd5e57fee09
Anuj Dubey <113243188+idkidgaf@users.noreply.github.com>
2022-11-02 19:50:05 +0530

    Time: 11 ms (5.00%), Space: 42.9 MB (6.51%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -16,29 +16,40 @@
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         
-       Stack<TreeNode> s1 = new Stack<>();
-       Stack<TreeNode> s2 = new Stack<>();
-        s1.push(root1);
-        s2.push(root2);
-        while(!s1.isEmpty() && !s2.isEmpty()){
-            if(helper(s1) != helper(s2)){
-                return false;
-            }
-        }
-        return s1.isEmpty() && s2.isEmpty();
+    //    Stack<TreeNode> s1 = new Stack<>();
+    //    Stack<TreeNode> s2 = new Stack<>();
+    //     s1.push(root1);
+    //     s2.push(root2);
+    //     while(!s1.isEmpty() && !s2.isEmpty()){
+    //         if(helper(s1) != helper(s2)){
+    //             return false;
+    //         }
+    //     }
+    //     return s1.isEmpty() && s2.isEmpty();
+    // }
+    // public int helper(Stack<TreeNode> s){
+    //     while(true){
+    //         TreeNode node = s.pop();
+    //         if(node.left != null){
+    //             s.push(node.left);
+    //         }
+    //         if(node.right != null){
+    //             s.push(node.right);
+    //         }
+    //         if(node.left == null && node.right == null){
+    //             return node.val;
+    //         }
+    //     }
+        
+        return helper(root1).equals(helper(root2));
     }
-    public int helper(Stack<TreeNode> s){
-        while(true){
-            TreeNode node = s.pop();
-            if(node.left != null){
-                s.push(node.left);
-            }
-            if(node.right != null){
-                s.push(node.right);
-            }
-            if(node.left == null && node.right == null){
-                return node.val;
-            }
+    public String helper(TreeNode root){
+        if(root == null){
+            return "";
         }
+        if(root.left == null && root.right == null){
+            return root.val+"-";
+        }
+        return helper(root.left) + helper(root.right);
     }
 }
