8b59e5d760c301282edec7bf7ecd5c4de5bbdbd8
Anuj Dubey <113243188+danuj828@users.noreply.github.com>
2022-12-03 00:05:49 +0530

    Time: 177 ms (7.11%), Space: 72.9 MB (6.17%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,19 +1,39 @@
 class Solution {
     public int minSteps(String s, String t) {
-        int m = s.length();
-        int n = t.length();
+        
+        //simple solution.
+        
+        // int m = s.length();
+        // int n = t.length();
+        // int ans = 0;
+        // if(s.equals(t)){
+        //     return 0;
+        // }
+        // int[] arr = new int[26];
+        // for(int i = 0; i<m; i++){
+        //     arr[s.charAt(i)-'a']++;
+        //     arr[t.charAt(i)-'a']--;
+        // }
+        // for(int i: arr){
+        //     if(i>0){
+        //         ans += i;
+        //     }
+        // }
+        // return ans;
+        
+        
+        //Using hashmap
         int ans = 0;
-        if(s.equals(t)){
-            return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
-        int[] arr = new int[26];
-        for(int i = 0; i<m; i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
-        }
-        for(int i: arr){
-            if(i>0){
-                ans += i;
+        for(int i = 0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch) && map.get(ch)>0){
+                map.put(ch, map.get(ch)-1);
+            } else {
+                ans++;
             }
         }
         return ans;
